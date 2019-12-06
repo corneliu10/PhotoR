fc3cdd3a0ab037c377f099544633a952aee3b765
Corneliu Dumitru <corneliu.dcv@gmail.com>
2019-12-06 14:24:05 +0200

    added model Album

---
+++ PhotoR/Models/Album.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhotoR.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
+    }
+}
