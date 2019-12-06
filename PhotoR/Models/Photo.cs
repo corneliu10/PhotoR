0cda90d321393a75f0784e0eec196f6ab0ec5a63
Corneliu Dumitru <corneliu.dcv@gmail.com>
2019-12-06 19:06:37 +0200

    added photo Controller + CRUD

--- PhotoR/Models/Photo.cs
+++ PhotoR/Models/Photo.cs
@@ -15,7 +15,7 @@ namespace PhotoR.Models
 
         public string Description { get; set; }
 
-        public int UserId { get; set; }
+        public string UserId { get; set; }
         
         public virtual ApplicationUser User { get; set; }
 
