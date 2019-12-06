43b1f996cb8f036744bfa95d5799d1202f8380d8
Calin <calin.costea@gmail.com>
2019-12-06 12:58:25 +0200

    Comment Model

---
+++ PhotoR/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhotoR.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public String Content { get; set; }
+    }
+}
